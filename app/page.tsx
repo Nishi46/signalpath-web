a46f94a00d983cd924772633110d662c681b7189
Nishi46 <nishigoldy@gmail.com>
2026-03-22 20:53:36 -0400

    calendly signup

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function LandingPage() {
         <p className='text-gray-400 text-sm mt-4'>
           Or{' '}
           <a
-            href='https://calendly.com/YOUR_USERNAME/demo'
+            href='https://calendly.com/nishigoldy/demo'
             className='text-indigo-600 hover:underline'
             target='_blank'
             rel='noopener noreferrer'
